9f7d8f7894c97feb4bc44a87d2b4c9be1cf98ab7
GuineaCoding <andrian.barbulat@gmail.com>
2025-10-02 22:17:16 +0100

    latest adjsutments to deploy to render

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 @app.route('/')
 def index():
